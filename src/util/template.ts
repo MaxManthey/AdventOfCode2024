594c0f4f7876417974c9c6158d8a83ecbfd0fcf7
Max Manthey <mantheymax23@gmail.com>
2024-12-06 10:06:30 +0100

    Day5

---
+++ src/util/template.ts
@@ -0,0 +1,10 @@
+import { getInput } from "../util";
+
+const input = getInput("DayX", "");
+
+function part1(document: string[]) {}
+
+function part2(document: string[]) {}
+
+console.log(part1(input));
+console.log(part2(input));
